52eeb3ceda7784d49f410f39442032fc072b49f0
John O'Donnell <john.t.odonnell9@gmail.com>
2022-01-10 18:00:49 +0000

    update server

--- src/server/sigserver.mjs
+++ src/server/sigserver.mjs
@@ -1,22 +1,22 @@
 // sigserver.mjs
 // Copyright (C) 2021 John T. O'Donnell
 // email: john.t.odonnell9@gmail.com
-// License: GNU GPL Version 3 or later. See Sigma16/README.md, LICENSE.txt
+// License: GNU GPL Version 3 or later. Sigma16/README.md, LICENSE.txt
 
 // This file is part of Sigma16.  Sigma16 is free software: you can
-// redistribute it and/or modify it under the terms of the GNU General
-// Public License as published by the Free Software Foundation, either
-// version 3 of the License, or (at your option) any later version.
-// Sigma16 is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.  You should have received
-// a copy of the GNU General Public License along with Sigma16.  If
-// not, see <https://www.gnu.org/licenses/>.
+// redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option)
+// any later version.  Sigma16 is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE.  See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with Sigma16.  If not, see <https://www.gnu.org/licenses/>.
 
-//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // SigServer
-//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 
 // Sigma16 can run either in a browser which provides a graphical user
 // interface (GUI), or in a shell which provides a text interface.
@@ -32,14 +32,15 @@
 // server for Sigma16, and it uses environment variables to adapt to
 // either Heroku or a local machine.
 
-// Sigma16 won't run on github pages, because github.io provides only
-// a static server.  Sigma16 uses concurrent processes with shared
-// memory, and (from May 2021) browsers require cross origin isolation
-// in order to use shared memory.  Since github.io does not serve
-// pages with the required headers, it doesn't support Sigma16.  In
-// contrast, this program is a web server that enforces cross origin
-// isolation, so it works for Sigma16.  It can run on a local computer
-// for offline testing, or on an Internet server for production use.
+// Sigma16 won't run on github pages, because github.io provides
+// only a static server.  Sigma16 uses concurrent processes with
+// shared memory, and (from May 2021) browsers require cross origin
+// isolation in order to use shared memory.  Since github.io does
+// not serve pages with the required headers, it doesn't support
+// Sigma16.  In contrast, this program is a web server that enforces
+// cross origin isolation, so it works for Sigma16 using a modern
+// browser.  It can run on a local computer for offline testing, or
+// on an Internet server for production use.
 
 // To use a local server:
 //    - execute `node src/server/runserver.mjs
@@ -47,27 +48,41 @@
 // To use the Internet server:
 //    - visit https://sigma16.herokuapp.com/...Sigma16 path...
 
-// The server supports the following Sigma16 URLs, where ... is either
-//    - https://sigma16.herokuapp.com/
-//    - http://localhost:3000/
-//    Return version number of the latest release:
-//       .../status/latest/i.j.k
-//       where i.j.k is the version number of the build making the request
-//    Launch the latest release:
-//       .../build/release/Sigma16/Sigma16.html
-//    Launch the development version:
-//       .../build/dev/Sigma16/Sigma16.html
-//    Launch specific version, e.g. 3.4.0
-//       .../build/3.4.0/Sigma16/Sigma16.html
-//    Index page with general information
-//       .../
-//    Testing the server
-//      ../hello.html
-//      ../world.html
+//-----------------------------------------------------------------------
+// URLs
+//-----------------------------------------------------------------------
 
-//-----------------------------------------------------------------------------
+// The server supports the following URLs, where xxx is either
+//    https://sigma16.herokuapp.com/
+//    http://localhost:3000/
+// --- rather than xxx denotes deprecated URL
+
+// Index page with general information
+//    xxx/
+
+// Return version number of the latest release of Sigma16, where
+// i.j.k is the version number of the build making the request
+//    xxx/sigma16/status/latest/i.j.k            *** NEW
+
+// Launch specified release of Sigma16
+//    xxx/sigma16/run                            *** NEW Latest release
+//    xxx/sigma16/dev                            *** NEW dev version
+//    xxx/sigma16/test                           *** NEW release candidate
+//    xxx/sigma16/version/VERSION               *** NEW launch specific version
+
+// Testing the server
+//   xxx/pseudopassword/hello.html               *** deprecated
+//   xxx/pseudopassword/world.html
+
+// DEPRECATED
+//    ---/status/latest/i.j.k                    *** deprecated, retain for now
+//    ---/build/release/Sigma16/Sigma16.html     *** OLD deprecated
+//    ---/build/dev/Sigma16/Sigma16.html         *** deprecated
+//    ---/build/3.4.0/Sigma16/Sigma16.html       *** deprecated
+
+//-----------------------------------------------------------------------
 // Packages
-//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 
 import express from 'express';
 import * as http from 'http'
@@ -77,9 +92,9 @@ import * as ejs from 'ejs'
 import * as fs from "fs";
 import { fileURLToPath } from 'url';
 
-//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // Configuration
-//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 
 // On the local server, the environment variables are set in .bashrc.
 // On Heroku, they are set using heroku config
@@ -91,12 +106,19 @@ import { fileURLToPath } from 'url';
 // These are typically the same in production but different for testing
 // S16_DEV_VERSION is substituted for 'dev' in http path
 
+// To set the environment variables:
+//   heroku login
+//   heroku config:set FOOBAR=VALUE   -- set environment variable
+//   heroku config                    -- show configuration variables
+
+// Look up the environment variables
+
+// Environment variables: versions
 const S16_LATEST_RELEASE = process.env.S16_LATEST_RELEASE
 const S16_RELEASE_VERSION = process.env.S16_RELEASE_VERSION
 const S16_DEV_VERSION = process.env.S16_DEV_VERSION
 
-// Server configuration
-
+// Environment variables: Server configuration
 const S16_LOCAL_PORT = process.env.S16_LOCAL_PORT
 const S16_RUN_ENV = process.env.S16_RUN_ENV
 const S16_SERVER_DIR = path.dirname (fileURLToPath (import.meta.url))
@@ -117,9 +139,9 @@ let S16_BUILD_DIR
 
 const PORT = process.env.PORT || S16_LOCAL_PORT
 
-//----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // Server
-//----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 
 const app = express();
 app.set ('view engine', 'ejs')
@@ -127,19 +149,19 @@ express.static.mime.define({'application/javascript': ['js']});
 express.static.mime.define({'text/css': ['css']});
 express.static.mime.define({'text/html': ['html']});
 
-//----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // Top index
-//----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 
 app.get ('/', (req,res) => {
     console.log (`responding-/`)
     res.sendFile (path.join (S16_BUILD_DIR, 'index.html'))
 })
 
-//----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // Provide latest version on request
 // URL path: status/latest/i.j.k
-//----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 
 // When Sigma16 initializes, it makes an http fetch to
 // /status/latest/i.j.k, where i.j.k identifies the running version.
@@ -149,6 +171,7 @@ app.get ('/', (req,res) => {
 // see whether they are running the latest release.
 
 
+// deprecated, use following version, remove this eventually...
 app.get ('/status/latest/:callerversion', (req,res) => {
     const reqInfo = {
         date: new Date (),
@@ -164,10 +187,25 @@ app.get ('/status/latest/:callerversion', (req,res) => {
     res.send (reply)
 })
 
-//----------------------------------------------------------------------------
+app.get ('/sigma16/status/latest/:callerversion', (req,res) => {
+    const reqInfo = {
+        date: new Date (),
+        ip: req.ip,
+        path: req.path,
+        callerversion: req.params.callerversion
+    }
+    const xs = JSON.stringify (reqInfo)
+    console.log (`responding-sigma16-status-latest ${xs}`)
+    res.type ('text/plain')
+    res.set ('Access-Control-Allow-Origin', '*')
+    const reply = S16_LATEST_RELEASE
+    res.send (reply)
+})
+
+//-----------------------------------------------------------------------
 // Request to launch Sigma16
-// URL path: build/:version/Sigma16/Sigma16.html
-//----------------------------------------------------------------------------
+// URL path: sigma16/build/:version/Sigma16/Sigma16.html
+//-----------------------------------------------------------------------
 
 // Calculate actual version number.  The http request may ask for a
 // specific version (1.2.3) or a symbolically named version (release,
@@ -176,7 +214,8 @@ app.get ('/status/latest/:callerversion', (req,res) => {
 
 function substituteVersion (v) {
     return v === 'release' ? S16_RELEASE_VERSION
-        : v === 'dev' ? S16_DEV_VERSION
+        : v === 'test' ? S16_DEV_VERSION
+        : v === 'dev' ? 'dev'
         : v
 }
 
@@ -191,7 +230,8 @@ function finish (req, res, loc) {
 
 // start page
 
-app.get('/build/:version/Sigma16/Sigma16.html', (req, res) => {
+
+app.get('/sigma16/build/:version/Sigma16/Sigma16.html', (req, res) => {
     const raw_v = req.params.version
     const v = substituteVersion (raw_v)
     const loc = path.join (S16_BUILD_DIR, v, 'Sigma16', 'Sigma16.html')
@@ -201,7 +241,7 @@ app.get('/build/:version/Sigma16/Sigma16.html', (req, res) => {
 
 // emwt
 
-app.get('/build/:version/Sigma16/emwt.mjs', (req, res) => {
+app.get('/sigma16/build/:version/Sigma16/emwt.mjs', (req, res) => {
     const raw_v = req.params.version
     const v = substituteVersion (raw_v)
     const loc = path.join (S16_BUILD_DIR, v, 'Sigma16',
@@ -212,7 +252,7 @@ app.get('/build/:version/Sigma16/emwt.mjs', (req, res) => {
 
 // emulator core
 
-app.get('/build/:version/Sigma16/emcore.wasm', (req, res) => {
+app.get('/sigma16/build/:version/Sigma16/emcore.wasm', (req, res) => {
     console.log ('responding-emcore.wasm')
     const raw_v = req.params.version
     const v = substituteVersion (raw_v)
@@ -225,7 +265,7 @@ app.get('/build/:version/Sigma16/emcore.wasm', (req, res) => {
 
 // generic file paths
 
-app.get('/build/:version/Sigma16/:a/:b/:c/*', (req, res) => {
+app.get('/sigma16/build/:version/Sigma16/:a/:b/:c/*', (req, res) => {
     const v = substituteVersion (req.params.version)
     const loc = path.join (S16_BUILD_DIR, v, 'Sigma16',
                            req.params.a,
@@ -235,7 +275,7 @@ app.get('/build/:version/Sigma16/:a/:b/:c/*', (req, res) => {
     finish (req, res, loc)
 })
 
-app.get('/build/:version/Sigma16/:a/:b/*', (req, res) => {
+app.get('/sigma16/build/:version/Sigma16/:a/:b/*', (req, res) => {
     const v = substituteVersion (req.params.version)
     const loc = path.join (S16_BUILD_DIR, v, 'Sigma16',
                            req.params.a,
@@ -244,7 +284,7 @@ app.get('/build/:version/Sigma16/:a/:b/*', (req, res) => {
     finish (req, res, loc)
 })
 
-app.get('/build/:version/Sigma16/:a/*', (req, res) => {
+app.get('/sigma16/build/:version/Sigma16/:a/*', (req, res) => {
     const v = substituteVersion (req.params.version)
     const loc = path.join (S16_BUILD_DIR, v, 'Sigma16',
                            req.params.a,
@@ -258,16 +298,16 @@ app.get('/build/:version/Sigma16/:a/*', (req, res) => {
 // Sigma16/src/base).  They are provided by this rule, which must come
 // after the rules that match src/gui/* and src/base/*
 
-app.get('/build/:version/Sigma16/*.mjs', (req, res) => {
+app.get('/sigma16/build/:version/Sigma16/*.mjs', (req, res) => {
     const v = substituteVersion (req.params.version)
     const loc = path.join (S16_BUILD_DIR, v, 'Sigma16',
                            'src', 'base', path.basename (req.path))
     finish (req, res, loc)
 })
 
-//----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // Cross origin isolation
-//----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 
 // app.use (cors ())
 // app.use (express.static ('public'))
@@ -292,11 +332,11 @@ app.get('/build/:version/Sigma16/*.mjs', (req, res) => {
 // Cross-Origin-Resource-Policy header or CORS headers
 // (Access-Control-Allow-* and so forth).
 
-//----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // Testing the server
 // URL path: hello.html
 // URL path: world.html
-//----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 
 app.get ('/hello.html', (req,res) => {
     res.render ('hello')
@@ -306,9 +346,9 @@ app.get ('/world.html', (req,res) => {
     res.render ('world')
 })
 
-//----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // Launch the server
-//----------------------------------------------------------------------------
+//-----------------------------------------------------------------------
 
 // The build directory contains a set of subdirectories, one for each
 // build that can be launched.  The subdirectories are named with the
@@ -324,13 +364,15 @@ export function StartServer () {
         console.log ('Running on Internet server')
         // Find the directory this program is running in and use that to
         // find the build directory
-        S16_BUILD_DIR = path.join (S16_SERVER_DIR, '..', '..', 'build')
+        S16_BUILD_DIR = path.join (S16_SERVER_DIR, '..', '..',
+                                   'Sigma16', 'build')
     } else if (S16_RUN_ENV === 'Local') {
         console.log ('Running on local development machine')
-        S16_BUILD_DIR = path.join (process.env.S16PART1,
-                                   process.env.S16PART2,
-                                   process.env.S16PART3,
-                                   'Sigma16-builds', 'build')
+        S16_BUILD_DIR = process.env.S16_LOCAL_BUILD_DIR
+//        S16_BUILD_DIR = path.join (process.env.SIGPART1,
+//                                   process.env.SIGPART2,
+//                                   process.env.SIGPART3,
+//                                   'Sigma16', 'build')
         console.log (`Local build directory = ${S16_BUILD_DIR}`)
     } else {
         console.log (`Server error: cannot find build directory for ${S16_RUN_ENV}`)
